694e6cf543069f703c0a9aaedbe23170b65e48af
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2025-04-10 20:03:28 +0200

    Crear clase Player con nombre y ficha

--- Connecta4/src/Player.java
+++ Connecta4/src/Player.java
@@ -1,2 +1,17 @@
 public class Player {
+    private final String name;
+    private final Token token;
+
+    public Player(String name, String symbol) {
+        this.name = name;
+        this.token = new Token(symbol);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Token getToken() {
+        return token;
+    }
 }
