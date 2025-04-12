9502b4e8425e3566feaa36944baf1a7e008165dd
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2025-04-12 23:36:10 +0200

    Modificaciones de mejor en las clases game y connecta4

--- Connecta4/src/Game.java
+++ Connecta4/src/Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Game {
     private Board board;
     private Player[] players;
@@ -18,12 +19,23 @@ public class Game {
             Player player = players[currentPlayer];
             System.out.println(player.getName() + " (" + player.getToken().getSymbol() + "), elige una columna (0-6):");
 
-            int column = scanner.nextInt();
+            int column;
+            while (true) {
+                try {
+                    column = Integer.parseInt(scanner.nextLine());
+                    if (column >= 0 && column < 7) break;
+                } catch (NumberFormatException ignored) {}
+                System.out.println("Entrada inválida. Inténtalo de nuevo:");
+            }
 
             if (board.dropToken(column, player.getToken().getSymbol())) {
                 if (board.checkWin(player.getToken().getSymbol())) {
                     board.printBoard();
                     System.out.println("¡" + player.getName() + " ha ganado!");
+                    break;
+                } else if (board.isFull()) {
+                    board.printBoard();
+                    System.out.println("¡Empate! El tablero está lleno.");
                     break;
                 }
                 currentPlayer = 1 - currentPlayer;
